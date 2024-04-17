0a42be9c24674b04bec89fd44fa5a9a3a49c4b39
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-04-17 12:23:41 +0500

    complate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StageModule } from './stage/stage.module';
+import { Stage } from './stage/entities/stage.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'hardwork',
+      entities: [Stage],
+      synchronize: true,
+    }),
+    StageModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 3000
+  app.setGlobalPrefix('api');
+  await app.listen(PORT, (): void => {
+    console.log(`http://127.0.0.1:${PORT}`);
+  });
 }
 bootstrap();
